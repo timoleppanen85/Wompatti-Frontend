4c900c828ad022f0d2c4c58764e2e4a0b1e1b73d
Mikko Kaipainen <31957407+MikkoTKaipainen@users.noreply.github.com>
2019-10-21 14:26:50 +0300

    RouterModule
    Added routes and canActivate[]

---
+++ src/app/admin/question-set/question-set.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-question-set',
+  templateUrl: './question-set.component.html',
+  styleUrls: ['./question-set.component.css']
+})
+export class QuestionSetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ResultDataComponent } from './result-data/result-data.component';
+import {RouterModule} from '@angular/router';
+import { QuestionsComponent } from './admin/questions/questions.component';
+import {AdminAuthGuardService} from './admin-auth-guard.service';
+import { QuestionSetComponent } from './admin/question-set/question-set.component';
 
 @NgModule({
   declarations: [
@@ -12,9 +16,35 @@ import { ResultDataComponent } from './result-data/result-data.component';
     LoginComponent,
     HomeComponent,
     ResultDataComponent,
+    QuestionsComponent,
+    QuestionSetComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'result-data',
+        component: ResultDataComponent
+      },
+      {
+        path: 'admin/questions',
+        component: QuestionsComponent,
+        canActivate: [AdminAuthGuardService]
+      },
+      {
+        path: 'admin/questions-set',
+        component: QuestionSetComponent,
+        canActivate: [AdminAuthGuardService]
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]

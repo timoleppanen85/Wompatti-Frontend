1e1d0c55077c1cd2104abac591324ebe10dcfeb2
Mikko Kaipainen <31957407+MikkoTKaipainen@users.noreply.github.com>
2019-10-21 13:49:34 +0300

    C&S
    Basic components and services that are vital to authorization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ResultDataComponent } from './result-data/result-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ResultDataComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/result-data/result-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-result-data',
+  templateUrl: './result-data.component.html',
+  styleUrls: ['./result-data.component.css']
+})
+export class ResultDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

4a9155800e097e0ca50b872038b73465d41e224a
Mikko Kaipainen <31957407+MikkoTKaipainen@users.noreply.github.com>
2020-01-08 13:48:33 +0200

    dialog done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ResultDataComponent } from './result-data/result-data.component';
 import { QuestionsComponent } from './questions/questions.component';
-import {MatButtonModule, MatFormFieldModule, MatIconModule, MatMenuModule, MatProgressBarModule} from '@angular/material';
+import {MatButtonModule, MatDialogModule, MatFormFieldModule, MatIconModule, MatMenuModule, MatProgressBarModule} from '@angular/material';
 import {AppRoutingModule, routingComponents} from './app-routing-module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { MenuComponent } from './menu/menu.component';
 import { BottomNavComponent } from './bottom-nav/bottom-nav.component';
 import { LogoComponent } from './logo/logo.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HintDialogComponent } from './hint-dialog/hint-dialog.component';
+
 
 @NgModule({
   declarations: [
@@ -26,6 +28,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MenuComponent,
     BottomNavComponent,
     LogoComponent,
+    HintDialogComponent,
   ],
   imports: [
     BrowserModule,
@@ -36,6 +39,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatProgressBarModule,
+    MatDialogModule
+  ],
+  entryComponents: [
+  HintDialogComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material';
+import {HintDialogComponent} from '../hint-dialog/hint-dialog.component';
+import data from '../assets/workquestions.json';
+
+
 
 @Component({
   selector: 'app-questions',
@@ -7,8 +12,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionsComponent implements OnInit {
 
-  constructor() { }
+  questions: any = data;
 
-  ngOnInit() {
+  constructor(public dialog: MatDialog) {
+  }
+
+  openHint(): void {
+    const dialogRef = this.dialog.open(HintDialogComponent, {
+      position: {top: '50px', right: '100px'}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
+  ngOnInit(): void {
   }
 }
+

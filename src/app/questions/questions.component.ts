c00d2aa78768ca75c3152324ac69637f71c1695b
Mikko Kaipainen <31957407+MikkoTKaipainen@users.noreply.github.com>
2020-02-01 18:35:44 +0200

    question iteration works

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -13,9 +13,14 @@ import {ActivatedRoute, Router} from '@angular/router';
   styleUrls: ['./questions.component.css']
 })
 export class QuestionsComponent implements OnInit {
-  public selectedId;
-  question: any; // = data[0].questions[0].question;
-  answer: any; // = data[0].questions[0].options;
+  selectedId;
+  questionIndex = 0;
+  question: any;
+  options: any;
+  subject: any;
+  image: any;
+  currentQuestion: any[];
+  currentOptions: any[] = this.options;
 
   constructor(public dialog: MatDialog, private router: Router, private activatedRoute: ActivatedRoute) {
   }
@@ -24,8 +29,10 @@ export class QuestionsComponent implements OnInit {
     this.activatedRoute.paramMap.subscribe(params => {
       this.selectedId = params.get('id');
     });
+    this.subject = data[this.selectedId].subject;
     this.question = data[this.selectedId].questions[0].question;
-    this.answer = data[this.selectedId].questions[0].options;
+    this.options = data[this.selectedId].questions[0].options;
+    this.image = data[this.selectedId].img;
   }
 
   openHint(): void {
@@ -38,24 +45,93 @@ export class QuestionsComponent implements OnInit {
     });
   }
 
-  nextQuestion() {
-    // TODO for statement iterating through questions and saving results.
-    const currentQuestion: any = data[this.selectedId].questions[0].question;
-    console.log(currentQuestion);
-    for (let i = 0; i < currentQuestion.length; i++) {
-      console.log(i);
-      if (i === +1) {
-        console.log(i);
-        this.question = data[this.selectedId].questions[i].question;
-        return;
-      }
+
+  showQuestion() {
+    this.currentQuestion = data[this.selectedId].questions[this.questionIndex].question;
+    if (this.questionIndex === data[this.selectedId].questions[0].question) {
+      this.currentQuestion = data[this.selectedId].questions[this.questionIndex].question;
     }
+    return this.currentQuestion;
   }
 
-// this.question = data[this.selectedId].questions[i].question;
-// if (currentQuestion === currentQuestion.length) {
-// console.log(currentQuestion.length);
-// this.router.navigate(['/result-data']);
+  showOptions() {
+    this.currentOptions = data[this.selectedId].questions[this.questionIndex].question.options;
+    if (this.questionIndex === data[this.selectedId].questions[0].options[this.questionIndex]) {
+      this.currentQuestion = data[this.selectedId].questions[this.questionIndex].options[this.questionIndex];
+    }
+    return this.currentOptions;
+  }
 
+  nextQuestion() {
+    console.log(this.questionIndex);
+    console.log(data[this.selectedId].questions.length);
+    if (this.questionIndex <= data[this.selectedId].questions.length) {
+      this.questionIndex++;
+    } else {
+      this.router.navigate(['/result-data']);
+    }
+    return this.currentQuestion;
+  }
 
 }
+
+
+//     do {
+//       for (let = 0;
+//
+//       i < this
+//
+//         .currentQuestion;
+//     .
+//       length;
+//       ++
+//     )
+//       {
+//         console;
+//       .
+//
+//         log(i);
+//
+//         if (i
+//
+//           ===
+//           1{
+//           ;
+//         }
+//       )
+//         {
+//           console;
+//         .
+//
+//           log(i);
+//
+//           this;
+//         .
+//           question = data[this.selectedId].questions[i].question;
+//           return;
+//         }
+//       }
+//     }
+//     while (currentQuestion === currentQuestion.length);
+//     {
+//       ;
+//     }
+//     if (currentQuestion === currentQuestion.length) {
+//       this.router.navigate(['/result-data']);
+//     }
+//   }
+//
+// //   for (let j = 0; j < currentQuestion.length; j++) {
+// //     if (j === 2) {
+// //       this.question = data[this.selectedId].questions[i].question;
+// //     }
+// //   }
+// //   if (i === currentQuestion.length) {
+// //     this.router.navigate(['/result-data']);
+// //   }
+// // }
+//
+// // this.question = data[this.selectedId].questions[i].question;
+// // if (currentQuestion === currentQuestion.length) {
+// // console.log(currentQuestion.length);
+// this.router.navigate(['/result-data']);

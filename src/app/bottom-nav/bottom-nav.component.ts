2e502c823e0a7b06cdec27c3bdcd8ea310c4b2ab
Mikko Kaipainen <31957407+MikkoTKaipainen@users.noreply.github.com>
2020-02-06 20:55:18 +0200

    quick fixes

--- src/app/bottom-nav/bottom-nav.component.ts
+++ src/app/bottom-nav/bottom-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 
 @Component({
@@ -8,9 +8,14 @@ import {Router} from '@angular/router';
 })
 export class BottomNavComponent implements OnInit {
 
-  constructor(public router: Router) { }
+  constructor(public router: Router) {
+  }
 
   ngOnInit() {
+  }
+
+  results() {
+    this.router.navigate(['/result-data']);
   }
 
 }
